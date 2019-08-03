5e6b35b440f838833ef707b2e62e374f1ed519cf
LinuxUser404 <nikolay.pershyn@gmail.com>
2019-08-04 01:52:15 +0300

    linting

--- app/sudoku-solver.hs
+++ app/sudoku-solver.hs
@@ -13,10 +13,9 @@ main :: IO ()
 main = putStr . toString . solve =<< getSudoku where
   getSudoku = do
     dimensions   <- getLine
-    square  <- getContents
-    return $ readPuzzle dimensions square
+    readPuzzle dimensions <$> getContents
 
 toString :: [SudokuPuzzle] -> String
 toString [] = "There are no solutions"
-toString (s:[]) = show s
-toString solutions = "There are " ++ (show $ length solutions) ++ " solutions:\n" ++ concatMap show solutions
+toString [s] = show s
+toString solutions = "There are " ++ show (length solutions) ++ " solutions:\n" ++ concatMap show solutions
--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -1,4 +1,5 @@
 -- Copyright (c) 2016, Mykola Pershyn
+{-# LANGUAGE TupleSections #-}
 module Sudoku
     ( readPuzzle
     , SudokuPuzzle(..)
@@ -21,7 +22,7 @@ import System.Exit(exitFailure, exitSuccess)
 --myDot = liftM2 (.)
 
 data SudokuPuzzle = GenPuzzle Size SudokuBoard deriving Eq
-data SudokuBoard  = GenBoard [Cell] deriving Eq
+newtype SudokuBoard  = GenBoard [Cell] deriving Eq
 type Candidate = (Int, Cell) -- (pos, possibleValue)
 type Candidates = (Int, [Cell]) -- (pos, possibleValues)
 type Size = (Int, Int)
@@ -37,23 +38,24 @@ test testPuzzle correctSolution = if correctSolution == testSolution then passed
   testSolution = solve testPuzzle
   showSolution [] = "No solutions\n"
   showSolution [x] = show x
-  showSolution xs = (show $ length xs) ++ " solutions found, expected: " ++ show (length correctSolution)
+  showSolution xs = show (length xs) ++ " solutions found, expected: " ++ show (length correctSolution)
 
 instance Show SudokuPuzzle where
-  show (GenPuzzle (x, y) (GenBoard board)) = unlines $ (unwords (map show [x, y])) : (map unwords . toRows . map showCell $ board) where
+  show (GenPuzzle (x, y) (GenBoard board)) = unlines $ unwords (map show [x, y]) : (map unwords . toRows . map showCell $ board) where
     toRows [] = []
-    toRows b = (take (x * y) b) : (toRows $ drop (x * y) b)
+    toRows b = take (x * y) b : toRows (drop (x * y) b)
 
 solve :: SudokuPuzzle -> [SudokuPuzzle]
 solve puzzle
-  | isValid    puzzle == False   = []       -- cuts invalid answers, test4 has a unique solution
-  | isFilled   puzzle            = [puzzle] -- solves test1
-  | isSolvable puzzle == False   = []       -- solves test3
-  | (hasSoleCandidates   puzzle) = solve . (flip updatePuzzle puzzle) . getSoleCandidates   $ puzzle -- solves test2
-  | (hasUniqueCandidates puzzle) = solve . (flip updatePuzzle puzzle) . getUniqueCandidates $ puzzle -- still not enough for test4
-  | otherwise                  = concat . map solve . derivePuzzles puzzle . head . getCandidates $ puzzle -- solves slowly(test4)
+  | not (isValid    puzzle)    = []       -- cuts invalid answers, test4 has a unique solution
+  |      isFilled   puzzle     = [puzzle] -- solves test1
+  | not (isSolvable puzzle)    = []       -- solves test3
+  | hasSoleCandidates   puzzle = solve . (`updatePuzzle` puzzle) . getSoleCandidates   $ puzzle -- solves test2
+  | hasUniqueCandidates puzzle = solve . (`updatePuzzle` puzzle) . getUniqueCandidates $ puzzle -- still not enough for test4
+  | otherwise                  = concatMap solve . derivePuzzles puzzle . head . getCandidates $ puzzle -- solves slowly(test4)
     where
-      hasSoleCandidates = (>0) . length . getSoleCandidates
+      hasSoleCandidates = not . null . getSoleCandidates
+      hasUniqueCandidates = not . null . getUniqueCandidates
 
 -- checks for duplicates that are > 0
 isValid :: SudokuPuzzle -> Bool
@@ -61,25 +63,21 @@ isValid puzzle = all (==False) . map check $ map getSet [Row, Col, Rect] where
   values = [1 .. n]
   setIDs = [0 .. n - 1]
   n = getPuzzleSize puzzle
-  check getSet' = any (==True) . map (any (>0) . (\\ values) . getSet' puzzle) $ setIDs
+  check getSet' = any (any (>0) . (\\ values) . getSet' puzzle) setIDs
 
 isFilled :: SudokuPuzzle -> Bool
-isFilled puzzle = all (>0) . getCells $ puzzle
+isFilled = all (>0) . getCells
 
 -- every empty cell should have a candidate
 isSolvable :: SudokuPuzzle -> Bool
-isSolvable puzzle = filter (\val -> snd val == []) (getCandidates puzzle) == []
-
-
-hasUniqueCandidates :: SudokuPuzzle -> Bool
-hasUniqueCandidates puzzle = (length . getUniqueCandidates) puzzle > 0
+isSolvable puzzle = not (any (null . snd) (getCandidates puzzle))
 
 getSoleCandidates :: SudokuPuzzle -> [Candidate]
 getSoleCandidates = filterSoleCandidates . getCandidates where
-  filterSoleCandidates candidates = map ( \ (n, i:_) -> (n,i) ) . filter ((==1) .length . snd) $ candidates
+  filterSoleCandidates = map ( \ (n, i:_) -> (n,i) ) . filter ((==1) .length . snd)
 
 derivePuzzles :: SudokuPuzzle -> Candidates -> [SudokuPuzzle]
-derivePuzzles puzzle (pos, vals) = map (flip updatePuzzle puzzle) . map (\ i -> [(pos, i)]) $ vals
+derivePuzzles puzzle (pos, vals) = map (flip updatePuzzle puzzle . (\i -> [(pos, i)])) vals
 
 getCells :: SudokuPuzzle -> [Cell]
 getCells (GenPuzzle _ (GenBoard board)) = board
@@ -92,9 +90,9 @@ getPuzzleDimY (GenPuzzle (_, y) _) = y
 getPuzzleSize (GenPuzzle (x, y) _) = x * y
 
 getSetIndicies :: SetType -> SudokuPuzzle -> Int -> [Int]
-getSetIndicies Row   puzzle              rowID  = map (\colID -> rowID * (getPuzzleSize puzzle) + colID) $ [0..(getPuzzleSize puzzle)-1]
-getSetIndicies Col   puzzle              colID  = map (\rowID -> rowID * (getPuzzleSize puzzle) + colID) $ [0..(getPuzzleSize puzzle)-1]
-getSetIndicies Rect (GenPuzzle (x, y) _) rectID = map (\rectElement -> getIndexOfElementInRectangle x y rectID rectElement) $ [0..x*y-1]
+getSetIndicies Row   puzzle              rowID  = map (\colID -> rowID * getPuzzleSize puzzle + colID) [0.. getPuzzleSize puzzle - 1]
+getSetIndicies Col   puzzle              colID  = map (\rowID -> rowID * getPuzzleSize puzzle + colID) [0.. getPuzzleSize puzzle - 1]
+getSetIndicies Rect (GenPuzzle (x, y) _) rectID = map (getIndexOfElementInRectangle x y rectID) [0..x*y-1]
 
 getSet :: SetType -> SudokuPuzzle -> Int -> [Cell]
 getSet s puzzle setID = map (getCells puzzle !!) $ getSetIndicies s puzzle setID
@@ -113,8 +111,8 @@ readPuzzle dimensions square = GenPuzzle (x, y) board
     board = (GenBoard . map readCell . words) square
 
 cellIDToSetID :: SetType -> Int -> SudokuPuzzle -> Int
-cellIDToSetID Row  cellID puzzle = cellID `quot` (getPuzzleSize puzzle)
-cellIDToSetID Col  cellID puzzle = cellID `rem`  (getPuzzleSize puzzle)
+cellIDToSetID Row  cellID puzzle = cellID `quot` getPuzzleSize puzzle
+cellIDToSetID Col  cellID puzzle = cellID `rem`  getPuzzleSize puzzle
 cellIDToSetID Rect cellID puzzle = rowID `quot` y * y + colID `quot` x
   where
     x = getPuzzleDimX puzzle
@@ -140,7 +138,7 @@ getCandidates puzzle = map (getCandidate puzzle) candidateCellIds
 
 -- for each row/col/rect checks whether a specific value can be assigned only to one cell in a unit
 getUniqueCandidates :: SudokuPuzzle -> [Candidate]
-getUniqueCandidates puzzle = sort . union [] . concat . (map myVal) $ [1..size]
+getUniqueCandidates puzzle = sort . union [] . concatMap myVal $ [1..size]
   where
     -- get indecies of a set, peek empty, get candidates for them, peek candidates with testVal
     -- in other words get candidates for a given set with testVal in them.
@@ -148,18 +146,18 @@ getUniqueCandidates puzzle = sort . union [] . concat . (map myVal) $ [1..size]
     cells = getCells puzzle
     filterUnknows = filter $ (==0) . (!!) cells -- filter indecies of unknown cells
     fullConstr :: Int -> [[(Int, [Int])]] -- for each set get candidates with testVal
-    fullConstr testVal = concat . map (myF testVal) $ [getSetIndicies Row  puzzle, getSetIndicies Col puzzle, getSetIndicies Rect puzzle]
+    fullConstr testVal = concatMap (myF testVal) [getSetIndicies Row  puzzle, getSetIndicies Col puzzle, getSetIndicies Rect puzzle]
     myF testVal indices = map (constr indices testVal) [0..size - 1] -- get candidates with val for each set.
     filterUnique = filter $ (==1) . length
     myVal :: Int -> [(Int, Int)] -- for each value get unique candidates
-    myVal testVal = map (flip (,) testVal . fst . head) . filterUnique . fullConstr $ testVal
+    myVal testVal = map ((, testVal) . fst . head) . filterUnique . fullConstr $ testVal
     size = getPuzzleSize puzzle
 
 updatePuzzle :: [Candidate] -> Update
 updatePuzzle theUpdates = foldl1 (.) $ map updateWith theUpdates where
   updateWith (pos, val) (GenPuzzle size (GenBoard oldBoard)) = GenPuzzle size (GenBoard newBoard) where
     newBoard = left ++ (val:right)
-    (left, (0:right)) = splitAt pos oldBoard -- the update shouldn't happen on a filled cell
+    (left, 0:right) = splitAt pos oldBoard -- the update shouldn't happen on a filled cell
 
 readCell :: String -> Cell
 readCell s
